db557abe4411abff77ed51a01017b4a720b14d54
Dustin Hurtt <dustin_hurtt@outlook.com>
2025-03-09 07:54:40 -0400

    added useEffect to Navbar to prevent infinite re-renders dustin 754392025

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../context/auth.context';
 
 const Navbar = () => {
   const { logOutUser, getToken, getUsername } = useContext(AuthContext);
-  const user = getUsername();
+  const [user, setUser] = useState('')
+
   console.log("this is the user ->", user);
+
+  useEffect(() => {
+    if(!user) {
+        let thisUser = getUsername()
+        setUser(thisUser)
+    }
+  }, [])
 
   return (
     <nav className='navbar'>
